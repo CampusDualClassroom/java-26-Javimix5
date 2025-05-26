f9758c8235fbcf0bb8551ef2ddfb826047020487
Javimix5 <javiusa2018@gmail.com>
2025-05-26 12:09:58 +0200

    Exercise26 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise26.java
+++ src/main/java/com/campusdual/classroom/Exercise26.java
@@ -3,6 +3,12 @@ package com.campusdual.classroom;
 
 public class Exercise26 {
     public static void main(String[] args) {
+        Phonebook phonebook = new Phonebook();
+
+        Contact c1 = new Contact("Javier","González Prados", "669332626");
+        phonebook.addContact(c1);
+
+        phonebook.mainMenu();
 
     }
 }
